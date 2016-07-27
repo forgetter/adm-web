56e7535ada7410a91c51f54a7befc11f34c4b4a5
fogray <forget871205@126.com>
2016-07-27 16:50:45 +0800

    Update shotcut.js

--- skins/js/shotcut.js
+++ skins/js/shotcut.js
@@ -1,1 +1,5 @@
-
+function cShortCutDiv(){
+  var div = '<div class="portlet"> <div class="portlet-title"><div class="pull-right" style="width:80%;text-align:right;"> <a href="https://dev.imaicloud.com/adm-web/" target="_adm" style="cursor:pointer;">ADM控制台>></a> </div> </div> </div>';
+  return div;
+}
+cShortCutDiv();
