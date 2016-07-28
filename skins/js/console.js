bd9f1d9de0ab24c984defb83adaf60a0fad86829
fogray <forget871205@126.com>
2016-07-28 09:03:34 +0800

    Update console.js

--- skins/js/console.js
+++ skins/js/console.js
@@ -3,6 +3,7 @@ $(function(){
     $('[data-toggle=confirmation]').confirmation({ container: 'body', btnOkClass: 'btn btn-sm btn-success', btnCancelClass: 'btn btn-sm btn-danger'});
     //获取cookie中用户上下文
     var userCtx = getCookie('imaicloud_payload');
+    userCtx = '{name:"anna"}';
     if (userCtx!= null && userCtx!="") {
     	var jsonUserCtx = eval('('+userCtx+')');
         $('#navUserName').html(jsonUserCtx.name);
