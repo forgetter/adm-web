ddfb777051564951882e6e9eb05ac70b8e76e2c3
fogray <forget871205@126.com>
2016-07-28 16:37:21 +0800

    Update shotcut.js

--- skins/js/shotcut.js
+++ skins/js/shotcut.js
@@ -1,12 +1,12 @@
 (function(){
     window.ADM_SHORTCUT = function(config){
         function initDom(){
-             var contaienr = $("#admShotcut");
-             if(contaienr.size() == 0){
-                throw "没有定义包装div";
+             var contaienr = document.getElementById('admShortcut');
+             if(!contaienr){
+                throw "没有定义ID为admShortcut的包装div";
              }
 			var div = '<div class="portlet"> <div class="portlet-title"><div class="pull-right" style="width:100%;text-align:right;"> <a href="https://dev.imaicloud.com/adm-web/" target="_adm" style="cursor:pointer;">ADM控制台>></a> </div> </div> </div>';
-             document.getElementById('admShotcut').appendChild(div);
+             contaienr.appendChild(div);
         }
         return {
             init:function(){
